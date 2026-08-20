7cf4717cd0d01ae7adf18471ce32068dd9b0b50d
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-08-20 13:15:58 +0000

    Fix filter argument handling

--- 20_08_2026_revision/filter.c
+++ 20_08_2026_revision/filter.c
@@ -3,9 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 
-int main(int ac, char *av)
+int main(int ac, char **av)
 {
-    if (av != 2)
+    if (av != 2 || !av[1][0])
         return 1;
     char *buf = NULL, *s = av[1], tmp[4096];
     int n, i, k, len = 0, cap = 0, sl = strlen(s);
@@ -28,6 +28,7 @@ int main(int ac, char *av)
     i = 0;
     while (i < len)
     {
+        // memmem(buf + i, len - i, s, sl) == buf + i;
         if (sl + i <= len && memmem(buf + i, sl, s, sl) == buf + i)
         {
             k = 0;
